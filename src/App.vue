aa541f6af5617ce4a1e74bfc1babb81d32aa6b7d
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-06-03 15:56:17 -0300

    :package: MartinsR Site-Portfólio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Master />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Master from '@/views/Master.vue';
 
-nav {
-  padding: 30px;
-}
+export default {
+  app: '',
+  components: {
+    Master
+  },
+  setup() {
+    return {
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    }
+  }
+};
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
+
+<style></style>
